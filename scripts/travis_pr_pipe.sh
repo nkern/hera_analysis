808dd78c24db506c773c1f0a2251dc89b9b87e2d
Nicholas Kern <nkern@berkeley.edu>
2018-05-07 01:28:24 -0700

    travis pr test 2

--- scripts/travis_pr_pipe.sh
+++ scripts/travis_pr_pipe.sh
@@ -5,6 +5,6 @@
 
 # Post a comment on the PR with pipeline output images
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": \"Hello world\"}" \
+-d "{\"body\": \"Here is a plot: \n ![PAPER_noise_sim](./scripts/paper_noise_sim.png)\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 
